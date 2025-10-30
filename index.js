57692d4ec2318cc1f44d458abce00c2883a75993
Arman Mir <mirarman8583@gmail.com>
2025-10-30 12:44:11 +0600

    MONGODB te toka dilam

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+// AnotherSimpleDBUSer
+// VLm3Trf8QObwH8tR
+const uri =
+  "mongodb+srv://AnotherSimpleDBUSer:VLm3Trf8QObwH8tR@anothersimplecrudprojec.sly0hz6.mongodb.net/?appName=AnotherSImpleCRUDProject";
+
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
 //middleware
 app.use(cors());
@@ -14,6 +29,25 @@ app.get("/", (req, res) => {
   res.send("Hello World : Another Simple CRUD server is running");
 });
 
+
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+        
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+
 app.listen(port, () => {
   console.log(`Another simple crud server is listening on port ${port}`);
 });
+ 
