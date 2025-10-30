2ffeaf724598666209fda26429f54b96c9829754
Arman Mir <mirarman8583@gmail.com>
2025-10-30 10:54:03 +0600

    basic server created

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World : Another Simple CRUD server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Another simple crud server is listening on port ${port}`);
-    
-})
+  console.log(`Another simple crud server is listening on port ${port}`);
+});
